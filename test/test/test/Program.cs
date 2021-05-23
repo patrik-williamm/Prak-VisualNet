531f22c1e7dda60dd7ae105966edce9af3e14bbb
patrik william <patrikwilliam001@gmail.com>
2021-05-23 17:02:32 +0800

    new file test/test/

---
+++ test/test/test/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Asus
+ * Date: 23/05/2021
+ * Time: 16:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace test
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("test git ");
+			Console.ReadKey(true);
+		}
+	}
+}
