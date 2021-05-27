99f1aafe1bd7b5813c73fd9d3e0ce5b11146feb8
patrik william <patrikwilliam001@gmail.com>
2021-05-27 11:59:22 +0800

    initial commit

---
+++ if_else/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Asus
+ * Date: 06/05/2021
+ * Time: 14:07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace if_else {
+	class Program {
+		public static void Main(string[] args) {
+			
+			float ipk;
+			string predikat;
+			Console.Write("Masukkan Ipk 	: ");
+			ipk = Convert.ToSingle(Console.ReadLine());
+			
+			if (ipk>3.5)
+				predikat = "cumlaude";
+			else if(ipk > 3)
+				predikat = "sangat memuaskan";
+			else if(ipk >= 2.75) 
+				predikat = "memuaskan";
+			else if(ipk >= 2)
+				predikat = "cukup";
+			else
+				predikat = "memuakkan";
+			
+			Console.WriteLine("predikat anda adalah : "+predikat);
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ switch/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Asus
+ * Date: 06/05/2021
+ * Time: 14:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace program3
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int ipk;
+			Console.Write("masukkan kode [1-5] : ");
+			ipk = Convert.ToInt16(Console.ReadLine());
+			
+			switch(ipk) {
+				case 1:
+					Console.WriteLine("sistem informasi ");
+					break;
+				case 2:
+					Console.WriteLine("teknik informatika");
+					break;
+				case 3:
+					Console.WriteLine("rekayasa perangkat lunak");
+					break;
+				case 4: 
+					Console.WriteLine("manajemen informatika");
+					break;
+				case 5 : 
+					Console.WriteLine("teknik sipil");
+					break;
+				default:
+					Console.WriteLine("code anda masukkan tidak di temukan!!!");
+					break;
+			}
+	
+			Console.ReadKey(true);
+		}
+	}
+}
