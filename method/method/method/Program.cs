dce9c0af8f7ffcb81bbef93da16bed758338dfdc
patrik william <patrikwilliam001@gmail.com>
2021-05-27 11:54:54 +0800

    initial commit

---
+++ method/method/method/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Asus
+ * Date: 27/05/2021
+ * Time: 11:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace method{
+	
+	class test{
+		private string nama;
+		private int age;
+		
+		public test() {
+		}
+		
+		public test(string nama="nama", int age = 0) {
+			this.nama = nama;
+			this.age = age;
+		}
+		private void setNama(string nama) {
+		 	this.nama = nama;
+		}
+		private string getNama() {
+			return nama;
+		}
+		
+		private void setAge(int age){
+			this.age = age;
+		}
+		
+		private int getAge(){
+			return age;
+		}
+		
+		
+//		private int testXY(int x, int y) {
+//			int total = x *  y;
+//			return total;
+//		}
+		
+		public static void Main(string[] args){
+			
+			test test1 = new test("patrik william", 20);
+			test1.setNama("Naruto Uzumaki");
+			test1.setAge(25);
+			Console.WriteLine(test1.getNama());
+			Console.WriteLine(test1.getAge());
+			Console.ReadKey(true);
+		}
+	}
+}
--- method/method/method/Properties/AssemblyInfo.cs
+++ method/method/method/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("appGui_1")]
+[assembly: AssemblyTitle("method")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("appGui_1")]
+[assembly: AssemblyProduct("method")]
 [assembly: AssemblyCopyright("Copyright 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
