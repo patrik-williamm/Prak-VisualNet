18e4da39d052ed44793157564e4f2fc4159baccc
patrik william <patrikwilliam001@gmail.com>
2021-05-28 13:25:07 +0800

    initial commit

---
+++ perulangan/foreach/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Asus
+ * Date: 28/05/2021
+ * Time: 13:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace foreach
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
